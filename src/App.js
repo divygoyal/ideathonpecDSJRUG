6749a20fdaab61e8501c7f7db453d81afeba205f
divygoyal <86590133+divygoyal@users.noreply.github.com>
2021-11-20 20:34:15 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -11,13 +11,13 @@ class App extends Component {
   }
 
   static getDerivedStateFromError(error) {
-    // Update state so the next render will show the fallback UI.
+    
     console.log('some error has occured');
     return { hasError: true };
   }
 
   componentDidCatch(error, info) {
-    // You can also log the error to an error reporting service
+    
     console.log(error, info);
   }
 
